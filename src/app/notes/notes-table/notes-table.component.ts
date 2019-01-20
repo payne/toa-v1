46c356a7ff403c3e3643c81ee6e0dd1265d42bd1
Matt Payne <Payne@MattPayne.org>
2019-01-20 11:48:08 -0600

    Start with https://angularfirebase.com/lessons/material-data-tables-with-firestore/

--- src/app/notes/notes-table/notes-table.component.ts
+++ src/app/notes/notes-table/notes-table.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
+
 
 @Component({
-  selector: 'notes-table',
-  templateUrl: './notes-table.component.html',
-  styleUrls: ['./notes-table.component.scss']
+  selector: 'data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
 })
-export class NotesTableComponent implements OnInit {
+export class DataTableComponent implements AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['name', 'age', 'email', 'phrase', 'edit'];
+  dataSource: MatTableDataSource<any>; 
 
-  ngOnInit() {
+  @ViewChild(MatSort) sort: MatSort;
+  
+  constructor(private afs: AngularFirestore, public dialog: MatDialog) { }
+
+
+  ngAfterViewInit() {
+    this.afs.collection<any>('hackers').valueChanges().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); 
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  openDialog(data): void {
+    /*
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '350px',
+      data: data
+    });
+    */
+  }
+
+  trackByUid(index, item) {
+    return item.uid
   }
 
 }
