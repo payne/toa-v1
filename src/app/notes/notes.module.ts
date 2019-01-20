dfce046e68fb208a4155f8203f9fd9f71b177a4f
Matt Payne <Payne@MattPayne.org>
2019-01-20 11:42:56 -0600

    moved notes-table into notes folder

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { NotesListComponent } from './notes-list/notes-list.component';
 import { NoteDetailComponent } from './note-detail/note-detail.component';
 import { NotesService } from './notes.service';
-import { NotesTableComponent } from '../notes-table/notes-table.component';
+import { NotesTableComponent } from './notes-table/notes-table.component';
 
 @NgModule({
   imports: [
