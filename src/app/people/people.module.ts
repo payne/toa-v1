77bdfbf22197616f87617d8847043ea30ed67d69
Matt Payne <Payne@MattPayne.org>
2019-01-21 19:24:52 -0600

    Hand made new people module.

---
+++ src/app/people/people.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSortModule,
+  MatDialogModule,
+  MatButtonModule
+} from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule, BrowserAnimationsModule,
+    RouterModule, ReactiveFormsModule,
+    BrowserModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatDialogModule,
+    MatButtonModule,
+  ],
+  // declarations: [NotesListComponent, NoteDetailComponent, NotesTableComponent, ProfileComponent],
+  // providers: [NotesService]
+})
+export class NotesModule { }
