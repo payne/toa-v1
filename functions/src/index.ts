50d394f553a926c3bc1d2f1b17656c1b2bbbadd3
Matt Payne <Payne@MattPayne.org>
2019-01-23 21:00:47 -0600

    nodemailer used to send emails from firebase functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+const nodemailer = require('nodemailer');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
