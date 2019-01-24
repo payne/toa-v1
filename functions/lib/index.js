d2645d3dbf70473727f92a072852187efdf72e4f
Matt Payne <Payne@MattPayne.org>
2019-01-23 21:42:01 -0600

    wip function to send email does not work, yet.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,6 +1,15 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
+const nodemailer = require('nodemailer');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -12,6 +21,51 @@ exports.newUserData = functions.auth.user().onCreate((userRecord, context) => {
     const email = userRecord.email;
     console.log(userId);
     console.log(email);
-    //return admin.database().ref(`users/${userId}`).
+    const displayName = userRecord.displayName; // The display name of the user.
+    // [END eventAttributes]
+    return sendWelcomeEmail(email, displayName);
 });
+// Start with https://github.com/firebase/functions-samples/blob/Node-8/quickstarts/email-users/functions/index.js
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
+});
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'Cloud Storage for Firebase quickstart';
+// [START sendWelcomeEmail]
+/**
+ * Sends a welcome email to new user.
+// [START onCreateTrigger]
+export const sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+// [END onCreateTrigger]
+  // [START eventAttributes]
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName; // The display name of the user.
+  // [END eventAttributes]
+
+  return sendWelcomeEmail(email, displayName);
+});
+ */
+// [END sendWelcomeEmail]
+// Sends a welcome email to the given user.
+function sendWelcomeEmail(email, displayName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const mailOptions = {
+            from: `${APP_NAME} <noreply@firebase.com>`,
+            to: email,
+            subject: `Welcome to ${APP_NAME}!`,
+            text: `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`,
+        };
+        // The user subscribed to the newsletter.
+        yield mailTransport.sendMail(mailOptions);
+        console.log('New welcome email sent to:', email);
+        return null;
+    });
+}
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,7 +14,10 @@ export const newUserData = functions.auth.user().onCreate((userRecord, context) 
   const email = userRecord.email; 
   console.log(userId);
   console.log(email);
-  //return admin.database().ref(`users/${userId}`).
+  const displayName = userRecord.displayName; // The display name of the user.
+  // [END eventAttributes]
+
+  return sendWelcomeEmail(email, displayName);
 });
 
 
@@ -36,7 +39,6 @@ const APP_NAME = 'Cloud Storage for Firebase quickstart';
 // [START sendWelcomeEmail]
 /**
  * Sends a welcome email to new user.
- */
 // [START onCreateTrigger]
 export const sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 // [END onCreateTrigger]
@@ -47,48 +49,21 @@ export const sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 
   return sendWelcomeEmail(email, displayName);
 });
+ */
 // [END sendWelcomeEmail]
 
-// [START sendByeEmail]
-/**
- * Send an account deleted email confirmation to users who delete their accounts.
- */
-// [START onDeleteTrigger]
-export const sendByeEmail = functions.auth.user().onDelete((user) => {
-// [END onDeleteTrigger]
-  const email = user.email;
-  const displayName = user.displayName;
-
-  return sendGoodbyeEmail(email, displayName);
-});
-// [END sendByeEmail]
 
 // Sends a welcome email to the given user.
 async function sendWelcomeEmail(email, displayName) {
   const mailOptions = {
     from: `${APP_NAME} <noreply@firebase.com>`,
     to: email,
+    subject: `Welcome to ${APP_NAME}!`,
+    text:  `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`,
   };
 
   // The user subscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
   await mailTransport.sendMail(mailOptions);
   console.log('New welcome email sent to:', email);
-  return null;
-}
-
-// Sends a goodbye email to the given user.
-async function sendGoodbyeEmail(email, displayName) {
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email,
-  };
-
-  // The user unsubscribed to the newsletter.
-  mailOptions.subject = `Bye!`;
-  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log('Account deletion confirmation email sent to:', email);
   return null;
 }
